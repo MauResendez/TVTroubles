451dc97cfb9b05627547329985f19cc4077eeb8b
nhn190000 <77559898+nhn190000@users.noreply.github.com>
2021-03-23 15:28:27 -0500

    Add color scheme to navbar and change size of text

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,32 +6,32 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink
+  NavLink,
 } from 'reactstrap';
 
-const Example = (props) => {
+const MyNavBar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">TV Troubles</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Home</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/components/">Swipe</NavLink>
-            </NavItem>          
-          </Nav>
-        </Collapse>
-      </Navbar>
+        <Navbar color="info" light expand="md">
+          <NavbarBrand href="/"><h3 className="linkTextColor">TV Troubles</h3></NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/"><h5 className="linkTextColor">Home</h5></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/components/"><h5 className="linkTextColor">Swipe</h5></NavLink>
+              </NavItem>          
+            </Nav>
+          </Collapse>
+        </Navbar>
     </div>
   );
 }
 
-export default Example;
+export default MyNavBar;
