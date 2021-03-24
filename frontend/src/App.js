6f4aa0a1bb4f5667ae6ec2521c9499b7f4048535
Jason Kluge <jason.kluge5813@gmail.com>
2021-03-24 10:18:55 -0500

    First feature commit, list out features and some styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import LandingPage from './views/LandingPage'
+import Features from './components/Features'
 function App() {
   return (
-    <LandingPage/>
+    <div>
+      <Features/>
+      <LandingPage/>
+    </div>
   );
 }
 
---
+++ frontend/src/components/Features.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { List } from 'reactstrap'
+
+const listStyle = {
+    color: 'black',
+    padding: "25px 50px 100px 100px",
+    textAlign: "left",
+  };
+
+
+const headerStyle = {
+    margin: "25px 25px",
+    textAlign: "center",
+};
+
+const componentStyle = {
+    backgroundColor: "#F4EAE6",
+}
+ 
+function Features()
+{
+    return ( 
+        <div style={componentStyle}> 
+        <div className="container">
+            <div className="row">
+                <div className="col-3"></div>
+                <h1 style={headerStyle} className="col-6">Features</h1>
+                <div className="col-3"></div>
+            </div>
+            <div className="row">
+                <div className="col-md-1 col-lg-3"></div>
+                <List style={listStyle} className="col-md-10 col-lg-6">
+                    <li>Choosing yes or no on a TV show</li>
+                    <li>Viewing previews of the shows before choosing</li>
+                    <li>Keeping track of what matches you've had</li>
+                </List>
+                <div className="col-md-1 col-lg-3"></div>
+        </div>
+        </div>
+        </div>
+    );
+}
+
+export default Features
