05b46dcf18c9bade5161f49d01d85ecbd081adf0
Varsha George <varsha.george1902@gmail.com>
2021-03-24 19:44:57 -0500

    modified explanation component

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Explanation from '../components/Explanation'
 
 function LandingPage(){
     return(
         <div>
             {/* Insert your created components here */}
+            <Explanation/>
             <h1>Landing Page Component</h1>
         </div>
     )
