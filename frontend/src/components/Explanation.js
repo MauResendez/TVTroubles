a9e399f0b3f9ee632dfaf259a9292efd1e1cd3e8
codubee <info@codubee.com>
2021-03-25 12:03:57 -0500

    Removed comment and deprecated hedaer tag

--- frontend/src/components/Explanation.js
+++ frontend/src/components/Explanation.js
@@ -1,19 +1,18 @@
-// This is where the explanation component will be created
 import React from 'react';
 
-function Explanation(){
-    return(
-        <div class="jumbotron">   
-        <h1 class="display-4">Welcome to TV Troubles!</h1>
+function Explanation() {
+    return (
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome to TV Troubles!</h1>
             <p class="lead">We will help you solve the issue of not knowing what to watch on TV.</p>
-                <hr class="my-4"></hr>
-            <p> We will show you one show description at a time along with a preview video of the show or movie. 
+            <hr class="my-4"></hr>
+            <p> We will show you one show description at a time along with a preview video of the show or movie.
             You will then select yes or no to the show or movie.</p>
-    </div>
+        </div>
     )
 };
 
-export default Explanation; 
+export default Explanation;
 
 
 
--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -4,9 +4,7 @@ import Explanation from '../components/Explanation'
 function LandingPage(){
     return(
         <div>
-            {/* Insert your created components here */}
             <Explanation/>
-            <h1>Landing Page Component</h1>
         </div>
     )
 }
