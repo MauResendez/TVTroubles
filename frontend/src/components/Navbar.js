4a1aba70d6501b3e2f99b85b0b56678ef21cb77b
nhn190000 <77559898+nhn190000@users.noreply.github.com>
2021-03-25 12:00:05 -0500

    M11-1 navbar (#1)
    * Renamed PR template file
    
    * Add navbar component
    
    Using reactstrap, I added navbar component and added that component to app.js
    
    * Add color scheme to navbar and change size of text
    
    * Change component into a function and refactor code.
    
    I changed NavBar component into a function from its original constant function and changed the code around to clean it up.
    
    * Removed .js from file name
    
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>
    Co-authored-by: codubee <info@codubee.com>

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <div>
+        <Navbar color="info" light expand="md">
+          <NavbarBrand href="/"><h3 className="linkTextColor">TV Troubles</h3></NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/"><h5 className="linkTextColor">Home</h5></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/components/"><h5 className="linkTextColor">Swipe</h5></NavLink>
+              </NavItem>          
+            </Nav>
+          </Collapse>
+        </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
