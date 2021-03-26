0aff279a5eb502d2952463a9c5cbe10e5a5c4580
Jason Kluge <jason.kluge5813@gmail.com>
2021-03-25 23:16:24 -0500

    Commit 4, okay now ready for pull request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import LandingPage from './views/LandingPage'
-import Features from './components/Features'
+
 function App() {
   return (
     <div>
-      <Features/>
       <LandingPage/>
     </div>
   );
