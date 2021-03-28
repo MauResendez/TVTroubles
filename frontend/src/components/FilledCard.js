74c3dcbf81c07f50477497fac35e689176741ee6
Mauricio <mauricioresendez524@gmail.com>
2021-03-28 18:16:27 -0500

    Created styles folder & created FilledCard.css

--- frontend/src/components/FilledCard.js
+++ frontend/src/components/FilledCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Card, CardHeader, CardFooter, CardImg, Container } from 'reactstrap';
 
+import '../styles/FilledCard.css';
+
 function FilledCard(props) 
 {
     return (
-        // Takes props given from the each FilledCard component in the Team component to use for the card's title, subtitle, and text
+        // Takes props given from the each FilledCard component in the Team component to use for the card's header, image, and footer
         <Container> 
             <Card>
                 <CardHeader><strong>{props.name}</strong></CardHeader>
