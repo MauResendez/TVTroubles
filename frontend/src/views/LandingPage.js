138e7692514ec2baac97f7471dc2b64b1db8d14c
MauResendez <43194324+MauResendez@users.noreply.github.com>
2021-03-29 10:59:30 -0500

    MW-5 Team (#4)
    * Renamed PR template file
    
    * Added Team component
    
    * Added inline style to CSS file and removed comment
    
    * Created styles folder & created FilledCard.css
    
    * Changed years in school
    
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Team from '../components/Team';
 import Explanation from '../components/Explanation'
 
 function LandingPage(){
     return(
         <div>
+            <Team/>
             <Explanation/>
         </div>
     )
