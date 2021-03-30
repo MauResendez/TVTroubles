c9f9de150013818acc648ea6ed7a3a4ce6b30798
kehngid <77454384+kehngid@users.noreply.github.com>
2021-03-30 11:51:10 -0500

    Made pull request changes

--- frontend/src/components/Features.js
+++ frontend/src/components/Features.js
@@ -1,47 +1,28 @@
 import React from 'react'
 import { List } from 'reactstrap'
+import '../styles/Features.css'
 
-const listStyle = {
-    color: 'black',
-    padding: "25px 50px 100px 100px",
-    textAlign: "left",
-    fontSize: "30px",
-
-  };
-
-
-const headerStyle = {
-    margin: "25px 25px",
-    textAlign: "center",
-    fontSize: "45px",
-};
-
-const componentStyle = {
-    backgroundColor: "#F4EAE6",
-    fontFamily: "Verdana",
-    borderTop: "50px solid #4297A0"
-}
- 
+//List 
 function Features()
 {
     return ( 
-        <div style={componentStyle}> 
-        <div className="container">
-            <div className="row">
-                <div className="col-3"></div>
-                <h1 style={headerStyle} className="col-6">Features</h1>
-                <div className="col-3"></div>
+        <div> 
+            <div className="container">
+                <div className="row"> 
+                    <div className="col-3"></div>
+                    <h1 className="col-6">Features</h1>
+                    <div className="col-3"></div>
+                </div>
+                <div className="row">
+                    <div className="col-md-1 col-lg-4"></div>
+                    <List className="col-md-10 col-lg-6">
+                        <li>Choosing yes or no on a TV show</li>
+                        <li>Viewing previews of the shows before choosing</li>
+                        <li>Keeping track of what matches you've had</li>
+                    </List>
+                    <div className="col-md-1 col-lg-4"></div>
+                </div>
             </div>
-            <div className="row">
-                <div className="col-md-1 col-lg-3"></div>
-                <List style={listStyle} className="col-md-10 col-lg-6">
-                    <li>Choosing yes or no on a TV show</li>
-                    <li>Viewing previews of the shows before choosing</li>
-                    <li>Keeping track of what matches you've had</li>
-                </List>
-                <div className="col-md-1 col-lg-3"></div>
-        </div>
-        </div>
         </div>
     );
 }
