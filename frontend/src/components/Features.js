06410472cf3ff03f881851327c0b4663c5bc32c8
kehngid <77454384+kehngid@users.noreply.github.com>
2021-04-03 10:39:29 -0500

    Fixed styling and comments

--- frontend/src/components/Features.js
+++ frontend/src/components/Features.js
@@ -2,21 +2,15 @@ import React from 'react'
 import { List } from 'reactstrap'
 import '../styles/Features.css'
 
-//List 
 function Features()
 {
     return ( 
+            <div className ="featuresBackground">
             <div className="container">
-                <div className="row"> 
-                {/* Displays title of component "Features" */}
-                    <div className="col-3"></div>
-                    <h1 className="col-6">Features</h1> 
-                    <div className="col-3"></div>
-                </div>
+                    <h1 className="featuresTitle">Features</h1> 
                 <div className="row">
-                    {/* Displays the list of features */}
                     <div className="col-md-1 col-lg-4"></div>
-                    <List className="col-md-10 col-lg-6">
+                    <List className="col-md-10 col-lg-6 featuresListItems">
                         <li>Choosing yes or no on a TV show</li>
                         <li>Viewing previews of the shows before choosing</li>
                         <li>Keeping track of what matches you've had</li>
@@ -24,6 +18,7 @@ function Features()
                     <div className="col-md-1 col-lg-4"></div>
                 </div>
             </div>
+        </div>
     );
 }
 
