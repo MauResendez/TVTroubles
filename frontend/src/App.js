1b6b662a52794d680b09be63e42474e668a40ec3
nhn190000 <77559898+nhn190000@users.noreply.github.com>
2021-04-05 10:34:28 -0500

    Formatted code to adhere to rest of code base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,20 @@ import './App.css';
 import LandingPage from './views/LandingPage';
 import Swipe from "./views/Swipe";
 import NavBar from './components/Navbar';
-import {Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 
 function App() {
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <Switch>
-          <Route exact path="/">
-            <LandingPage />
-          </Route>
-          <Route path="/swipe">
-            <Swipe />
-          </Route>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route path="/swipe">
+          <Swipe />
+        </Route>
       </Switch>
     </div>
   );
--- frontend/src/views/Swipe.js
+++ frontend/src/views/Swipe.js
@@ -3,7 +3,7 @@ import React from "react";
 function Swipe() {
     return (
         <h1>Swipe Page</h1>
-    ); 
+    );
 }
 
 export default Swipe;
