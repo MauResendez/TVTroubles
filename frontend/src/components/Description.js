10406bc00d1c108bda85af963881a3842bbf9c87
kehngid <77454384+kehngid@users.noreply.github.com>
2021-04-08 10:10:44 -0500

    Adding formatting to center description and allow text to wrap around

--- frontend/src/components/Description.js
+++ frontend/src/components/Description.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import '../styles/Description.css'
 
 function Description(props) {
     return (
-        <div>
-            <p>Name:{props.name}
-            <br></br>Description:{props.description}
-            <br></br>Rating:{props.rating}</p>
+        <div className="centersDescription">
+            <div className="descriptionComponenets">
+                <p>Name: {props.name}</p>
+                <p>Description: {props.description}</p>
+                <p>Rating: {props.rating}</p>
+            </div>
         </div>
     )
 };
 
 export default Description;
+
+/*
+    <p className="descriptionGeneral">Name:{props.name}
+    <br></br>Description:{props.description}
+     <br></br>Rating:{props.rating}</p> 
+
+     <div style={{textAlign: "center"}}>
+            <div style={{display: "inline-block", textAlign: "left"}}>
+
+
+    <div className= "descriptionLeftAlign">
+                Name:{props.name}<br />
+                Description:{props.description}<br />
+                Rating:{props.rating}<br />
+            </div>
+*/
