033f1c5fe3bb682a99360539da0e45702b8d5ea4
Varsha George <varsha.george1902@gmail.com>
2021-04-20 23:40:09 -0500

    component with preview video

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -2,13 +2,18 @@ import React from 'react';
 import Features from '../components/Features'
 import Team from '../components/Team';
 import Explanation from '../components/Explanation'
+import ResponsivePlayer from '../components/video/ResponsivePlayer'
 
-function LandingPage(){
-    return(
+function LandingPage() {
+    return (
         <div>
             <Explanation/>
             <Features/>
             <Team/>
+            {/* Insert your created components here */}
+            <ResponsivePlayer/>
+            <Explanation />
+            <h1>Landing Page Component</h1>
         </div>
     )
 }
