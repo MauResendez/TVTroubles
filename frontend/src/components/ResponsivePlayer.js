017166051e8b8a5ce3f3e869e842689387d874e6
codubee <info@codubee.com>
2021-04-21 10:23:14 -0500

    Added props to the player

--- frontend/src/components/ResponsivePlayer.js
+++ frontend/src/components/ResponsivePlayer.js
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 import { Row, Col } from 'reactstrap'
 
-function ResponsivePlayer() {
+function ResponsivePlayer(props) {
     return (
         <Row>
             <Col sm={{ size: 12, offset: 2 }}>
                 <ReactPlayer
-                    url="https://www.youtube.com/watch?v=rJZ0eqyZEHg&ab_channel=TheHill"
+                    url={props.url}
                     controls={true}
                 />
-</Col>
-</Row>
+            </Col>
+        </Row>
 
     )
 }
