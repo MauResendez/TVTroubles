7383719f5f526d764cdb4cb8495118b0ce2ac166
kehngid <77454384+kehngid@users.noreply.github.com>
2021-04-26 13:55:31 -0500

    Added post /addUserMovie to index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ app.use(express.json());
     when building your APIs. I suggest that you try to understand
     what each line is doing. Reach out if you have any questions.
 */
+/*
 app.get('/exampleApi', function (req, res) {
 
     axios.get('https://api.agify.io?name=Mike')
@@ -18,12 +19,44 @@ app.get('/exampleApi', function (req, res) {
         res.status(200).json(response.data);
     })
     .catch(function (error) {
-        /* 
-            It is good practice to handle the error and send back the 
-            error message that was received from the API call
-        */ 
+        
+            //It is good practice to handle the error and send back the 
+            //error message that was received from the API call
+        
         console.log(error)
         res.status(400).json({error:"An error occurred"});
+    })
+})*/
+
+/*
+{
+    "id":1,
+    "record": {
+        "name": "Zootopia",
+        "description": "Determined to prove herself, Officer Judy Hopps, the first bunny on Zootopia's police force, jumps at the chance to crack her first case - even if it means partnering with scam-artist fox Nick Wilde to solve the mystery.",
+        "rating": 7.7,
+        "preview_video":"url"
+    }
+}
+*/
+
+app.post('/addUserMovie', function (req, res) {
+    console.log(req.body)
+
+    const body = {
+        id: req.body.id,
+        record: req.body.record
+    }
+
+    axios.post('https://codubee-projects-api.herokuapp.com/tvTroubles/addUserMovie',body)
+
+    .then(function (response) {
+         console.log(response.data);
+         res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+        console.log(error)
+        res.status(400).json({error:"An error occurred with addUserMovie"})
     })
 })
 
