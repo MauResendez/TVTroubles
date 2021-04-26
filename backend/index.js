9d2eae82c2f78bbb34eaa75eae4bad5d22d52b8e
kehngid <77454384+kehngid@users.noreply.github.com>
2021-04-26 15:39:43 -0500

    Cleaned up comments

--- backend/index.js
+++ backend/index.js
@@ -3,43 +3,6 @@ const app = express()
 const axios = require('axios');
 app.use(express.json());
 
-
-/*
-    I will leave this here as an example that you can use
-    when building your APIs. I suggest that you try to understand
-    what each line is doing. Reach out if you have any questions.
-*/
-/*
-app.get('/exampleApi', function (req, res) {
-
-    axios.get('https://api.agify.io?name=Mike')
-    .then(function (response) {
-        // handle success and send back a 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
-    })
-    .catch(function (error) {
-        
-            //It is good practice to handle the error and send back the 
-            //error message that was received from the API call
-        
-        console.log(error)
-        res.status(400).json({error:"An error occurred"});
-    })
-})*/
-
-/*
-{
-    "id":1,
-    "record": {
-        "name": "Zootopia",
-        "description": "Determined to prove herself, Officer Judy Hopps, the first bunny on Zootopia's police force, jumps at the chance to crack her first case - even if it means partnering with scam-artist fox Nick Wilde to solve the mystery.",
-        "rating": 7.7,
-        "preview_video":"url"
-    }
-}
-*/
-
 app.post('/addUserMovie', function (req, res) {
     console.log(req.body)
 
