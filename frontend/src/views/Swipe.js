b2b6b168cad32a19326de1a0249f3d0a42311f85
codubee <info@codubee.com>
2021-04-28 16:37:13 -0500

    Moved buttons to swipe view

--- frontend/src/views/Swipe.js
+++ frontend/src/views/Swipe.js
@@ -1,18 +1,27 @@
 import React from "react";
 import MatchResults from "../components/MatchResults";
-import {matchesData} from '../data/matchesData'
+import { matchesData } from '../data/matchesData'
 import Description from '../components/Description';
-import {Container } from 'reactstrap'
-import YesNoButton from '../components/YesNoButton';
+import { Container, Button } from 'reactstrap'
+import '../styles/Swipe.css'
 
-function Swipe() {
-    return (
-        <Container>
-            <Description/>
-            <MatchResults data={matchesData}/>
-            <YesNoButton/>
-        </Container>
-    );
+class Swipe extends React.Component {
+
+    submit(){
+        alert('hello world')
+    }
+    render() {
+        return (
+            <Container>
+                <Description />
+                <div class="text-center">
+                    <Button color="success" onClick={this.submit} >Yes</Button>
+                    <Button color="danger" onClick={this.submit} className="ml-sm">No</Button>
+                </div>
+                <MatchResults data={matchesData} />
+            </Container>
+        )
+    }
 }
 
 export default Swipe;
