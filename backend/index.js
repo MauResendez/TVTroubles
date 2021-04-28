4ebe2f8c24ca9f16951e41fcaf706403e2967017
nhn190000 <77559898+nhn190000@users.noreply.github.com>
2021-04-28 16:27:20 -0500

    Add backend route for description (#10)

--- backend/index.js
+++ backend/index.js
@@ -27,4 +27,18 @@ app.get('/exampleApi', function (req, res) {
     })
 })
 
+//Backend route to get the movie/show description
+app.get('/getDescription', function (req, res) {
+
+    axios.get('https://codubee-projects-api.herokuapp.com/tvTroubles/getDescription')
+    .then(function (response) {
+        console.log(response.data);
+        res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+        console.log(error)
+        res.status(400).json({error:"An error occurred"});
+    })
+})
+
 app.listen(8080, () => console.log('Listening at locahost:8080'))
