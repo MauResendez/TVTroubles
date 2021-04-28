8dfc127b8798aef5a04d187b380d772b6e51a2af
Jason <77454384+kehngid@users.noreply.github.com>
2021-04-28 16:44:41 -0500

    [MW-13] Add matches (#12)
    * Added post /addUserMovie to index.js
    
    * Cleaned up comments
    
    * Removed the console.log message
    
    Co-authored-by: codubee <info@codubee.com>

--- backend/index.js
+++ backend/index.js
@@ -3,27 +3,22 @@ const app = express()
 const axios = require('axios');
 app.use(express.json());
 
+app.post('/addUserMovie', function (req, res) {
 
-/*
-    I will leave this here as an example that you can use
-    when building your APIs. I suggest that you try to understand
-    what each line is doing. Reach out if you have any questions.
-*/
-app.get('/exampleApi', function (req, res) {
+    const body = {
+        id: req.body.id,
+        record: req.body.record
+    }
 
-    axios.get('https://api.agify.io?name=Mike')
+    axios.post('https://codubee-projects-api.herokuapp.com/tvTroubles/addUserMovie',body)
+
     .then(function (response) {
-        // handle success and send back a 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
+         console.log(response.data);
+         res.status(200).json(response.data);
     })
     .catch(function (error) {
-        /* 
-            It is good practice to handle the error and send back the 
-            error message that was received from the API call
-        */ 
         console.log(error)
-        res.status(400).json({error:"An error occurred"});
+        res.status(400).json({error:"An error occurred with addUserMovie"})
     })
 })
 
