1d9fdc69b0279527cc8f724bc827933b900557fc
nhn190000 <77559898+nhn190000@users.noreply.github.com>
2021-05-16 15:43:56 -0500

    add getDescription api call to swipe.js

--- frontend/src/views/Swipe.js
+++ frontend/src/views/Swipe.js
@@ -4,16 +4,41 @@ import { matchesData } from '../data/matchesData'
 import Description from '../components/Description';
 import { Container, Button } from 'reactstrap'
 import '../styles/Swipe.css'
+import axios from 'axios'
 
 class Swipe extends React.Component {
 
     submit(){
         alert('hello world')
     }
+
+    constructor(props) {
+        super(props);
+        this.state = { 
+            name: '',
+            description: '',
+            rating: '' 
+        };
+        
+
+    }
+
+    componentDidMount() {
+        axios.get('/getDescription')
+        .then((response) => {
+            console.log(response);
+            this.setState({
+                name: response.title,
+                description: response.overview,
+                rating: response.vote_average
+            })
+        })
+    }
+
     render() {
         return (
             <Container>
-                <Description />
+                <Description name={this.state.name} description={this.state.description} rating={this.state.rating} />
                 <div class="text-center">
                     <Button color="success" onClick={this.submit} >Yes</Button>
                     <Button color="danger" onClick={this.submit} className="ml-sm">No</Button>
