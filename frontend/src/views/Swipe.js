ff90800a4d49a161d5b648aa4d4a9dbd018df22b
nhn190000 <77559898+nhn190000@users.noreply.github.com>
2021-05-17 10:40:15 -0500

    MW-17-getDescriptionAPICall (#14)
    * add getDescription api call to swipe.js
    
    * Fix error
    
    * Changed props from name to title
    
    Co-authored-by: codubee <info@codubee.com>

--- frontend/src/views/Swipe.js
+++ frontend/src/views/Swipe.js
@@ -4,16 +4,35 @@ import { matchesData } from '../data/matchesData'
 import Description from '../components/Description';
 import { Container, Button } from 'reactstrap'
 import '../styles/Swipe.css'
+import axios from 'axios'
 
 class Swipe extends React.Component {
 
     submit(){
         alert('hello world')
     }
+
+    constructor(props) {
+        super(props);
+        this.state = { 
+            data: {}
+        };
+    }
+
+    componentDidMount() {
+        axios.get('/getDescription')
+        .then((response) => {
+            console.log(response);
+            this.setState({
+                data: response.data
+            })
+        })
+    }
+
     render() {
         return (
             <Container>
-                <Description />
+                <Description title={this.state.data.title} description={this.state.data.description} rating={this.state.data.rating} />
                 <div class="text-center">
                     <Button color="success" onClick={this.submit} >Yes</Button>
                     <Button color="danger" onClick={this.submit} className="ml-sm">No</Button>
