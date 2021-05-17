2a41bd6d26e2a147aed568b57c69413b32fb096c
kehngid <77454384+kehngid@users.noreply.github.com>
2021-05-17 10:55:10 -0500

    Creating method to send a post request with movie info for the yes button

--- frontend/src/views/Swipe.js
+++ frontend/src/views/Swipe.js
@@ -10,12 +10,36 @@ class Swipe extends React.Component {
     submit(){
         alert('hello world')
     }
+
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            
+        }
+        this.yesButtonApi = this.yesButtonApi.bind(this);
+    }
+
+    yesButtonApi() {
+        var id = Math.floor(Math.random()*500);
+        var body = {
+            id: id,
+            record: this.state.data
+        }
+
+        axios.post('/addUserMovie', body)
+
+        .then((response) => {
+            console.log(response);
+        })
+    }
+
     render() {
         return (
             <Container>
                 <Description />
                 <div class="text-center">
-                    <Button color="success" onClick={this.submit} >Yes</Button>
+                    <Button color="success" onClick={this.yesButtonApi} >Yes</Button>
                     <Button color="danger" onClick={this.submit} className="ml-sm">No</Button>
                 </div>
                 <MatchResults data={matchesData} />
