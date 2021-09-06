05dd944aadf1173f32b3846160f764dc4c200c1e
Mauricio <mauricioresendez524@gmail.com>
2021-09-06 02:27:29 -0500

    Testing

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const mysql = require('mysql2');
-const cors = require('cors');
-const path = require('path');
+// const dotenv = require('dotenv');
+// const mysql = require('mysql2');
+// const cors = require('cors');
+// const path = require('path');
 const app = express();
 
 app.use(express.json());
@@ -11,116 +11,116 @@ app.use(cors());
 // Load env
 dotenv.config({ path: './config.env' });
 
-const db = mysql.createPool
-({
-    user: `${process.env.USER}`,
-    host: `${process.env.HOST}`,
-    password: `${process.env.PASSWORD}`,
-    database: `${process.env.DATABASE}`,
-    connectionLimit: 10,
-    waitForConnections: true,
-});
+// const db = mysql.createPool
+// ({
+//     user: `${process.env.USER}`,
+//     host: `${process.env.HOST}`,
+//     password: `${process.env.PASSWORD}`,
+//     database: `${process.env.DATABASE}`,
+//     connectionLimit: 10,
+//     waitForConnections: true,
+// });
 
-app.get('/user', (req, res) => 
-{
-    try 
-    {
-        let id = req.query.id;
-        let admin = 0;
+// app.get('/user', (req, res) => 
+// {
+//     try 
+//     {
+//         let id = req.query.id;
+//         let admin = 0;
 
-        // Check database to see if there's a user with the same ID as the one given to us.
-        // If not, create a new user
+//         // Check database to see if there's a user with the same ID as the one given to us.
+//         // If not, create a new user
 
-        db.query("SELECT * FROM users WHERE id = ?", [id], (err, result) => 
-        {                    
-            if(result.length == 0)
-            {
-                db.query("INSERT INTO users (id, admin) VALUES (?, ?)", [id, admin]);
-            }
+//         db.query("SELECT * FROM users WHERE id = ?", [id], (err, result) => 
+//         {                    
+//             if(result.length == 0)
+//             {
+//                 db.query("INSERT INTO users (id, admin) VALUES (?, ?)", [id, admin]);
+//             }
 
-            return res.status(200).json({"result": result});
-        });
-    } 
-    catch (err) 
-    {
-        console.log(err);
-        return res.status(400).json({error: "An error occurred with user"});
-    }
-});
+//             return res.status(200).json({"result": result});
+//         });
+//     } 
+//     catch (err) 
+//     {
+//         console.log(err);
+//         return res.status(400).json({error: "An error occurred with user"});
+//     }
+// });
 
-app.post('/addMatch', (req, res) =>
-{
-    try 
-    {
-        let uid = req.body.uid;
-        let mid = req.body.mid;
+// app.post('/addMatch', (req, res) =>
+// {
+//     try 
+//     {
+//         let uid = req.body.uid;
+//         let mid = req.body.mid;
            
-        db.query("INSERT INTO matches (uid, mid) VALUES (?, ?)", [uid, mid], (err, result) => 
-        {
-            return res.status(200).json({"result": result});              
-        });
-    } 
-    catch (err) 
-    {        
-        console.log(err);
-        return res.status(400).json({error: "An error occurred with addMatch"});
-    }
-});
+//         db.query("INSERT INTO matches (uid, mid) VALUES (?, ?)", [uid, mid], (err, result) => 
+//         {
+//             return res.status(200).json({"result": result});              
+//         });
+//     } 
+//     catch (err) 
+//     {        
+//         console.log(err);
+//         return res.status(400).json({error: "An error occurred with addMatch"});
+//     }
+// });
 
-app.post('/deleteMatch', (req, res) =>
-{
-    try 
-    {
-        let uid = req.body.uid;
-        let mid = req.body.mid;
+// app.post('/deleteMatch', (req, res) =>
+// {
+//     try 
+//     {
+//         let uid = req.body.uid;
+//         let mid = req.body.mid;
            
-        db.query("DELETE FROM matches WHERE uid = ? AND mid = ?", [uid, mid], (err, result) => 
-        {
-            return res.status(200).json({"result": result});              
-        });
-    } 
-    catch (err) 
-    {        
-        console.log(err);
-        return res.status(400).json({error: "An error occurred with deleteMatch"});
-    }
-});
+//         db.query("DELETE FROM matches WHERE uid = ? AND mid = ?", [uid, mid], (err, result) => 
+//         {
+//             return res.status(200).json({"result": result});              
+//         });
+//     } 
+//     catch (err) 
+//     {        
+//         console.log(err);
+//         return res.status(400).json({error: "An error occurred with deleteMatch"});
+//     }
+// });
 
-app.get('/getMatches', (req, res) =>
-{
-    try 
-    {
-        let id = req.query.id;
+// app.get('/getMatches', (req, res) =>
+// {
+//     try 
+//     {
+//         let id = req.query.id;
 
-        db.query("SELECT * FROM media WHERE id IN (SELECT mid FROM matches WHERE uid = ?);", [id], (err, matches) => 
-        {
-            return res.status(200).json(matches);
-        });
-    } 
-    catch (err) 
-    {
-        console.log(err);
-        return res.status(400).json({error: "An error occurred with getMatches"});       
-    }
-});
+//         db.query("SELECT * FROM media WHERE id IN (SELECT mid FROM matches WHERE uid = ?);", [id], (err, matches) => 
+//         {
+//             return res.status(200).json(matches);
+//         });
+//     } 
+//     catch (err) 
+//     {
+//         console.log(err);
+//         return res.status(400).json({error: "An error occurred with getMatches"});       
+//     }
+// });
 
-app.get('/getMovie', (req, res) =>
-{
-    try 
-    {
-        let id = Math.floor(Math.random() * 50) + 1;
+// app.get('/getMovie', (req, res) =>
+// {
+//     try 
+//     {
+//         let id = Math.floor(Math.random() * 50) + 1;
 
-        db.query(`SELECT * FROM media WHERE id = ${id}`, (err, result) => 
-        {
-            return res.status(200).json(result[0]);
-        });
-    } 
-    catch (err) 
-    {
-        console.log(err);
-        return res.status(400).json({error: "An error occurred with getMovie"});
-    }
-});
+//         db.query(`SELECT * FROM media WHERE id = ${id}`, (err, result) => 
+//         {
+//             return res.status(200).json(result[0]);
+//         });
+//     } 
+//     catch (err) 
+//     {
+//         console.log(err);
+//         return res.status(400).json({error: "An error occurred with getMovie"});
+//     }
+// });
 
 if(process.env.NODE_ENV === 'production')
 {
---
+++ routes/routes.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const router = express.Router();
+const dotenv = require('dotenv');
+const mysql = require('mysql2');
+const cors = require('cors');
+const path = require('path');
+
+const db = mysql.createPool
+({
+    user: `${process.env.USER}`,
+    host: `${process.env.HOST}`,
+    password: `${process.env.PASSWORD}`,
+    database: `${process.env.DATABASE}`,
+    connectionLimit: 10,
+    waitForConnections: true,
+});
+
+router.get('/user', (req, res) => 
+{
+    try 
+    {
+        let id = req.query.id;
+        let admin = 0;
+
+        // Check database to see if there's a user with the same ID as the one given to us.
+        // If not, create a new user
+
+        db.query("SELECT * FROM users WHERE id = ?", [id], (err, result) => 
+        {                    
+            if(result.length == 0)
+            {
+                db.query("INSERT INTO users (id, admin) VALUES (?, ?)", [id, admin]);
+            }
+
+            return res.status(200).json({"result": result});
+        });
+    } 
+    catch (err) 
+    {
+        console.log(err);
+        return res.status(400).json({error: "An error occurred with user"});
+    }
+});
+
+router.post('/addMatch', (req, res) =>
+{
+    try 
+    {
+        let uid = req.body.uid;
+        let mid = req.body.mid;
+           
+        db.query("INSERT INTO matches (uid, mid) VALUES (?, ?)", [uid, mid], (err, result) => 
+        {
+            return res.status(200).json({"result": result});              
+        });
+    } 
+    catch (err) 
+    {        
+        console.log(err);
+        return res.status(400).json({error: "An error occurred with addMatch"});
+    }
+});
+
+router.post('/deleteMatch', (req, res) =>
+{
+    try 
+    {
+        let uid = req.body.uid;
+        let mid = req.body.mid;
+           
+        db.query("DELETE FROM matches WHERE uid = ? AND mid = ?", [uid, mid], (err, result) => 
+        {
+            return res.status(200).json({"result": result});              
+        });
+    } 
+    catch (err) 
+    {        
+        console.log(err);
+        return res.status(400).json({error: "An error occurred with deleteMatch"});
+    }
+});
+
+router.get('/getMatches', (req, res) =>
+{
+    try 
+    {
+        let id = req.query.id;
+
+        db.query("SELECT * FROM media WHERE id IN (SELECT mid FROM matches WHERE uid = ?);", [id], (err, matches) => 
+        {
+            return res.status(200).json(matches);
+        });
+    } 
+    catch (err) 
+    {
+        console.log(err);
+        return res.status(400).json({error: "An error occurred with getMatches"});       
+    }
+});
+
+router.get('/getMovie', (req, res) =>
+{
+    try 
+    {
+        let id = Math.floor(Math.random() * 50) + 1;
+
+        db.query(`SELECT * FROM media WHERE id = ${id}`, (err, result) => 
+        {
+            return res.status(200).json(result[0]);
+        });
+    } 
+    catch (err) 
+    {
+        console.log(err);
+        return res.status(400).json({error: "An error occurred with getMovie"});
+    }
+});
