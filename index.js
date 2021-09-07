6c338f0894484aeb1db54abfd811580c7f01e1d3
Mauricio <mauricioresendez524@gmail.com>
2021-09-07 02:54:51 -0500

    Testing

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require('path');
+const cors = require('cors');
 
 // Load env
 dotenv.config({ path: './config.env' });
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/', require('./routes/routes'));
 
-const port = process.env.PORT || 8080;
-
 if(process.env.NODE_ENV === 'production')
 {
-    app.use(express.static(path.join(__dirname, 'build'))); 
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
 
     app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Listening at port ${port}`);
