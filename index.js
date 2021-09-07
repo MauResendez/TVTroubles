edfd22fa09549b61e0bb607298cb1011ce3e44a4
Mauricio <mauricioresendez524@gmail.com>
2021-09-07 04:03:16 -0500

    Testing

--- index.js
+++ index.js
@@ -16,15 +16,7 @@ app.use('/', require('./routes/routes'));
 
 if(process.env.NODE_ENV === 'production')
 {
-    // app.use(express.static(path.join(__dirname, 'client', 'build')))
-
-    // app.get('/*', (req, res) => {
-    //     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    // });
-
-    // "heroku-postbuild": "cd client && npm install && npm run build"
-
-    app.use(express.static(path.join(__dirname, 'build')))
+    app.use(express.static(path.join(__dirname, 'build')));
 
     app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
